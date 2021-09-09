331f6c932e3814d245614afa88517e63a2d3a4ba
foreverfl <forever_fl@naver.com>
2021-09-09 17:44:57 +0900

    210909 17:40

---
+++ src/baekjoon/_10870.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class _10870 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int fibonacci[] = new int[a + 1]; // The 'a+1' is because the 'fibonacci[]' starts from the 'fibonacci[0].'
+		sc.close();
+
+		for (int i = 0; i < fibonacci.length; i++) {
+
+			if (i == 0)
+				fibonacci[0] = 0; // It prevents the 'outofboundexception' to occur.
+			else if (i == 1)
+				fibonacci[1] = 1;
+			else
+				fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+		}
+		System.out.println(fibonacci[a]);
+
+	}
+
+}
