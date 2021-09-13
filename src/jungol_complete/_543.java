7dc52129c64803118975581a07abebb112d62925
foreverfl <forever_fl@naver.com>
2021-09-13 19:16:10 +0900

    210913 19:00

---
+++ src/jungol_complete/_543.java
@@ -0,0 +1,20 @@
+package jungol_complete;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _543 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int input = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= input; i++) {
+			if(i%2 == 0) {
+				System.out.print(i + " ");
+			}
+		}
+
+	}
+
+}
