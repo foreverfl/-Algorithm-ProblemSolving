33f5ada59d37860b978e01fde1d6e7c676206794
foreverfl <forever_fl@naver.com>
2021-09-21 19:03:15 +0900

    210921 19:00

--- src/BOJ/Copied_BOJ_01339_WordsMathematics.java
+++ src/BOJ/Copied_BOJ_01339_WordsMathematics.java
@@ -11,27 +11,28 @@ public class Copied_BOJ_01339_WordsMathematics {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		String[] arr = new String[N];
-		int[] alpha = new int[26]; 
+		int[] alphabet = new int[26];
 		for (int i = 0; i < N; i++) {
 			arr[i] = sc.next(); // 단어 입력
 		}
 
 		for (int i = 0; i < N; i++) {
-			int temp = (int) Math.pow(10, arr[i].length() - 1);
+			int temp = (int) Math.pow(10, arr[i].length() - 1); // 맨 앞자리 temp에 저장, ABC라고 할때 A에 100저장
 			for (int j = 0; j < arr[i].length(); j++) {
-				alpha[(int) arr[i].charAt(j) - 65] += temp;
+				alphabet[(int) arr[i].charAt(j) - 65] += temp; // ABC라고 할 때 alphabet[0] =100, alphabet[1] = 10,
+																// alphabet[2] =1
 				temp /= 10;
 			}
 		}
 
-		Arrays.sort(alpha);
+		Arrays.sort(alphabet);
 		int index = 9;
 		int sum = 0;
-		for (int i = alpha.length - 1; i >= 0; i--) {
-			if (alpha[i] == 0) {
+		for (int i = alphabet.length - 1; i >= 0; i--) { // 앞쪽부터 9부터 대입시작
+			if (alphabet[i] == 0) {
 				break;
 			}
-			sum += alpha[i] * index;
+			sum += alphabet[i] * index;
 			index--;
 		}
 		System.out.println(sum);
