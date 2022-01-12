fb34d674079a3ca2515ab791f4528d6f405560a4
foreverfl <forever_fl@naver.com>
2022-01-12 22:53:44 +0900

    220112 23:00

--- src/BOJ/Copied_BOJ_1260_DFSAndBFS.java
+++ src/BOJ/Copied_BOJ_1260_DFSAndBFS.java
@@ -1,71 +1,175 @@
 package BOJ;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
 
 public class Copied_BOJ_1260_DFSAndBFS {
 
-	static int[][] check;
-	static boolean[] checked;
-	static int N;
-	static int M;
-	static int start;
+	@SuppressWarnings({ "unchecked", "unused" })
+	private void solve() {
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int v = sc.nextInt();
 
-	@SuppressWarnings("resource")
-	public static void main(String[] args) throws IOException {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M = sc.nextInt();
-		start = sc.nextInt();
+		// 양방향 그래프, 가중치 x
 
-		check = new int[1001][1001]; // The value of the 'check[][]' is '1001' to accept coordinates as it is.
-		checked = new boolean[1001];
+		int[][] a = new int[n + 1][n + 1];
+		ArrayList<Integer>[] list = (ArrayList<Integer>[]) new ArrayList[n+1];
 
-		for (int i = 0; i < M; i++) {
-			int x = sc.nextInt();
-			int y = sc.nextInt();
+		boolean[] c = new boolean[n + 1];
 
-			check[x][y] = check[y][x] = 1;
+		for (int i = 0; i < m; i++) {
+			int v1 = sc.nextInt();
+			int v2 = sc.nextInt();
+
+			a[v1][v2] = 1;
+			a[v2][v1] = 1;
 		}
 
-		dfs(start);
-
-		checked = new boolean[1001]; // initialization
+		// dfs(a,c,v);
+		dfs(a, c, v, true);
 		System.out.println();
+		Arrays.fill(c, false);
+		bfs(a, c, v);
 
-		bfs();
 	}
 
-	// using a recursive function
-	public static void dfs(int i) {
-		checked[i] = true;
-		System.out.print(i + " ");
+	// 재귀 DFS - 인접행렬
+	public static void dfs(int[][] a, boolean[] c, int v) {
+		int n = a.length - 1;
 
-		for (int j = 1; j <= N; j++) {
-			if (check[i][j] == 1 && checked[j] == false) {
-				dfs(j);
+		c[v] = true;
+		System.out.print(v + " ");
+
+		for (int i = 1; i <= n; i++) {
+
+			if (a[v][i] == 1 && !c[i]) {
+				dfs(a, c, i);
+			}
+
+		}
+
+	}
+
+	// 스택 DFS - 인접행렬
+	public static void dfs(int[][] a, boolean[] c, int v, boolean flag) {
+		Stack<Integer> stack = new Stack<>();
+		int n = a.length - 1;
+
+		stack.push(v);
+		c[v] = true;
+		System.out.print(v + " ");
+
+		while (!stack.isEmpty()) {
+			int vv = stack.peek();
+
+			flag = false;
+
+			for (int i = 1; i <= n; i++) {
+
+				if (a[vv][i] == 1 && !c[i]) {
+					stack.push(i);
+					System.out.print(i + " ");
+
+					c[i] = true;
+					flag = true;
+					break;
+				}
+
+			}
+
+			if (!flag) {
+				stack.pop();
+			}
+
+		}
+
+	}
+
+	// 큐 BFS - 인접행렬
+	public static void bfs(int[][] a, boolean[] c, int v) {
+		Queue<Integer> q = new LinkedList<>();
+		int n = a.length - 1;
+
+		q.add(v);
+		c[v] = true;
+
+		while (!q.isEmpty()) {
+
+			v = q.poll();
+			System.out.print(v + " ");
+			
+			for (int i = 1; i <= n; i++) {
+
+				if (a[v][i] == 1 && !c[i]) {
+					q.add(i);
+					c[i] = true;
+				}
+
 			}
 		}
+
 	}
 
-	public static void bfs() {
-		Queue<Integer> queue = new LinkedList<Integer>();
-		queue.offer(start);
-		checked[start] = true;
-		System.out.print(start + " ");
+	public static void main(String[] args) {
+		sc.init();
 
-		while (!queue.isEmpty()) {
-			int temp = queue.poll();
+		new Copied_BOJ_1260_DFSAndBFS().solve();
+	}
 
-			for (int j = 1; j <= N; j++) {
-				if (check[temp][j] == 1 && checked[j] == false) {
-					queue.offer(j);
-					checked[j] = true;
-					System.out.print(j + " ");
+	static class sc {
+		private static BufferedReader br;
+		private static StringTokenizer st;
+
+		static void init() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+			st = new StringTokenizer("");
+		}
+
+		static String readLine() {
+			try {
+				return br.readLine();
+			} catch (IOException e) {
+			}
+			return null;
+		}
+
+		static String readLineReplace() {
+			try {
+				return br.readLine().replaceAll("\\s+", "");
+			} catch (IOException e) {
+			}
+			return null;
+		}
+
+		static String next() {
+			while (!st.hasMoreTokens()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
 				}
 			}
+			return st.nextToken();
+		}
+
+		static long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		static int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		static double nextDouble() {
+			return Double.parseDouble(next());
 		}
 	}
 }
