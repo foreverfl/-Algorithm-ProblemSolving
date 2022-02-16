8b9764e6100e9c67ca8e3bdb5d7007f20a2db579
foreverfl <forever_fl@naver.com>
2022-02-17 01:43:09 +0900

    220217 01:40

---
+++ src/BOJ_Copied/Copied_BOJ_01783_IllKnight.java
@@ -0,0 +1,36 @@
+package BOJ_Copied;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Copied_BOJ_01783_IllKnight {
+
+	static int[][] map;
+	static int N, M;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		System.out.println(solve());
+	}
+
+	private static int solve() {
+		if (N == 1) // Moving is impossible
+			return 1;
+		// The number of Maximum moving is 4.
+		if (N == 2) // vertical limit
+			return Math.min(4, (M + 1) / 2);
+		if (M < 7) // horizontal limit: when the process 1~4 is finished, M ¡æ M+6
+			return Math.min(4, M);
+		return M - 2;
+	}
+}
+
+// reference: https://log-laboratory.tistory.com/80
