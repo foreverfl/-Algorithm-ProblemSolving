a27ddcf55e09b92701890b374b4c25354faf19b8
foreverfl <forever_fl@naver.com>
2022-02-22 16:49:59 +0900

    220222 16:50

---
+++ src/BOJ/BOJ_05565_Receipt.java
@@ -0,0 +1,22 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_05565_Receipt {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int total = Integer.parseInt(br.readLine());
+
+		int sum = 0;
+		int repeat = 9;
+		while (repeat-- > 0) {
+			sum += Integer.parseInt(br.readLine());
+		}
+
+		System.out.println(total - sum);
+	}
+
+}
---
+++ src/BOJ/BOJ_10886_NotCuteAndCute.java
@@ -0,0 +1,32 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_10886_NotCuteAndCute {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		int cnt_1 = 0;
+		int cnt_0 = 0;
+
+		while (N-- > 0) {
+			int vote = Integer.parseInt(br.readLine());
+
+			if (vote == 1)
+				cnt_1++;
+			else
+				cnt_0++;
+		}
+		
+		if(cnt_1>cnt_0)
+			System.out.println("Junhee is cute!");
+		else
+			System.out.println("Junhee is not cute!");
+
+	}
+
+}
