f106d69494a02fcd20de7b5c37ec523934469de6
foreverfl <forever_fl@naver.com>
2022-02-26 07:19:05 +0900

    220226 07:20

---
+++ src/BOJ/Copied_BOJ_05582_CommonPartialString.java
@@ -0,0 +1,28 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Copied_BOJ_05582_CommonPartialString {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String a = br.readLine();
+		String b = br.readLine();
+
+		int max = 0;
+		int[][] map = new int[a.length() + 1][b.length() + 1];
+		for (int i = 1; i <= a.length(); i++) {
+			for (int j = 1; j <= b.length(); j++) {
+				if (a.charAt(i - 1) == b.charAt(j - 1)) {
+					map[i][j] = map[i - 1][j - 1] + 1;
+					max = Math.max(max, map[i][j]);
+				}
+			}
+		}
+		System.out.println(max);
+	}
+}
+
+// reference: https://velog.io/@jkh9615/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EB%B0%B1%EC%A4%80-5582-%EA%B3%B5%ED%86%B5-%EB%B6%80%EB%B6%84-%EB%AC%B8%EC%9E%90%EC%97%B4-Java
---
+++ src/BOJ_Copied/Copied_BOJ_02156_TastingWine.java
@@ -0,0 +1,35 @@
+package BOJ_Copied;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Copied_BOJ_02156_TastingWine {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		int[] wine = new int[n + 1];
+		for (int i = 1; i < n + 1; i++) {
+			wine[i] = Integer.parseInt(br.readLine());
+		}
+
+		int[] dp = new int[n + 1];
+
+		dp[1] = wine[1];
+
+		if (n > 1)
+			dp[2] = wine[1] + wine[2];
+
+		for (int i = 3; i < n + 1; i++) {
+			dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + wine[i], dp[i - 3] + wine[i - 1] + wine[i]));
+		}
+
+		System.out.println(dp[n]);
+
+	}
+
+}
+
+// reference: https://st-lab.tistory.com/135
