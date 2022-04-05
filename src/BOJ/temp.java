e5c758df9bfccda88cb621b295e3835ec3b548ee
foreverfl <forever_fl@naver.com>
2022-04-05 11:24:05 +0900

    220405 11:20

--- src/BOJ/temp.java
+++ src/BOJ/temp.java
@@ -6,8 +6,6 @@ public class temp {
 
 	public static void main(String[] args) throws IOException {
 
-		long a = 9223372036854775807L;
-		long b = 9223372036854775807L;
-		System.out.println(a + b);
+		///
 	}
 }
