e2b41e7bb27eafe38f9e9a82056e2c8daaca9c18
foreverfl <forever_fl@naver.com>
2022-05-27 02:30:26 +0900

    220527 02:30

---
+++ src/BOJ/BOJ_09613_SumOfGCD.java
@@ -0,0 +1,49 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_09613_SumOfGCD {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		int T = Integer.parseInt(br.readLine());
+		while (T-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int[] arr = new int[n];
+			for (int i = 0; i < n; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+
+			long sum = 0;
+			for (int i = 0; i < n; i++) {
+				for (int j = i + 1; j < n; j++) {
+					int tmp = gcd(arr[i], arr[j]);
+					sum += tmp;
+				}
+			}
+			sb.append(sum).append('\n');
+		}
+
+		System.out.println(sb.toString());
+	}
+
+	private static int gcd(int a, int b) {
+
+		while (b != 0) {
+			int r = a % b; // r = remains
+
+			// GCD(a, b) = GCD(b, r)
+			a = b;
+			b = r;
+		}
+		return a;
+	}
+
+}
---
+++ src/BOJ/Copied_BOJ_15927_PalindromeIsNotAPalindrome.java
@@ -0,0 +1,35 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Copied_BOJ_15927_PalindromeIsNotAPalindrome {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+
+		int len = str.length();
+
+		boolean flag = false;
+		for (int i = 0; i < len / 2; i++) {
+			if (str.charAt(i) != str.charAt(len - i - 1)) { // not palindrome
+				System.out.println(len);
+				return;
+			}
+
+			else if (str.charAt(i) != str.charAt(i + 1)) // checking all characters is same
+				flag = true;
+		}
+
+		if (flag) // palindrome
+			System.out.println(len - 1);
+
+		else // All characters is same
+			System.out.println(-1);
+	}
+
+}
+
+// reference: https://velog.io/@pss407/%EB%B0%B1%EC%A4%8015927-%ED%9A%8C%EB%AC%B8%EC%9D%80-%ED%9A%8C%EB%AC%B8%EC%95%84%EB%8B%88%EC%95%BC
