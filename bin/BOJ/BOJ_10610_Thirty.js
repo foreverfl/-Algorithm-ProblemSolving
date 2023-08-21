231f254c19aa6c63f6269073175bf14edbcc8b10
foreverfl <forever_fl@naver.com>
2023-08-21 22:32:01 +0900

    remove bin and .settings

--- bin/BOJ/BOJ_10610_Thirty.js
+++
@@ -1,4 +0,0 @@
-let fs = require('fs');
-let input = fs.readFileSync(__dirname + '\\input.txt').toString().split(' ');
-let sc = parseInt(input[0]);
-
--- bin/BOJ/BOJ_11399_ATM.js
+++
@@ -1,7 +0,0 @@
-const input = require('fs').readFileSync(__dirname + '\\input.txt').toString().split('\n');
-const map_sorted = input[1].split(" ").sort(function (a, b) { return a - b });
-let sum = 0;
-for (let i = 0; i < map_sorted.length; i++) {
-    sum += map_sorted[i] * (map_sorted.length - i);
-}
-console.log(sum);
