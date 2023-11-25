530127c62be98ff996c20bfaf29625f3bb2a5c28
foreverfl <forever_fl@naver.com>
2023-11-25 19:19:44 +0900

    Delete study material

--- src/Java/String_repeat.java
+++
@@ -1,9 +0,0 @@
-package Java;
-
-public class String_repeat {
-    public static void main(String[] args) {
-        String str = "abc";
-        String repeated = str.repeat(3);
-        System.out.println(repeated); //
-    }
-}
